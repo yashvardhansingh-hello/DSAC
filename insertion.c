96f496fe0c15e0d9eff3e5b683958a8094b8e579
Yash Vardhan Singh <yashvardhansingh7465@gmail.com>
2023-09-11 11:38:01 +0530

    DSA classes 1) fixed qLLst errors 2) in insertion making code more readeable

--- insertion.c
+++ insertion.c
@@ -1,7 +1,8 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-void display(int arr[], int size){
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+void display(int arr[], int size)
+{
     for (int i = 0; i < size; i++)
     {
         printf("%d ", arr[i]);
@@ -9,34 +10,35 @@ void display(int arr[], int size){
     printf("\n");
 }
 
-void indInsertion(int arr[], int* Usize, int Tsize, int index, int value) {
+void indInsertion(int arr[], int *Usize, int Tsize, int index, int value)
+{
     if (*Usize == Tsize)
     {
         printf("err'arr is full.'");
         return;
     }
-    if (index<0 || index>=*Usize)
+    if (index < 0 || index >= *Usize)
     {
         printf("err'Index out of bounds.'");
         return;
     }
     for (int i = *Usize; i >= index; i--)
     {
-        arr[i] = arr[i-1];
+        arr[i] = arr[i - 1];
     }
-    
+
     arr[index] = value;
     (*Usize)++;
-
 }
 
-int indDeletion(int arr[], int *Usize, int Tsize, int index) {
+int indDeletion(int arr[], int *Usize, int Tsize, int index)
+{
     if (*Usize == 0)
     {
         printf("err'Arr is empty.'");
         return -1;
     }
-    if (index<0 || index>=*Usize)
+    if (index < 0 || index >= *Usize)
     {
         printf("err'Index out of bounds.'");
         return -1;
@@ -44,13 +46,14 @@ int indDeletion(int arr[], int *Usize, int Tsize, int index) {
     int temp = arr[index];
     for (int i = index; i <= *Usize; i++)
     {
-        arr[i] = arr[i+1];
+        arr[i] = arr[i + 1];
     }
     (*Usize)--;
     return temp;
 }
 
-int linearSearch(int arr[], int Usize, int value){
+int linearSearch(int arr[], int Usize, int value)
+{
     for (int i = 0; i < Usize; i++)
     {
         if (arr[i] == value)
@@ -61,25 +64,33 @@ int linearSearch(int arr[], int Usize, int value){
     return -1;
 }
 
-int binarySearch(int arr[], int Usize, int value) {
+int binarySearch(int arr[], int Usize, int value)
+{
     int low, high, mid;
     low = 0;
-    high = Usize-1;
-    mid = ceil((low+high)/2);
+    high = Usize - 1;
+    mid = ceil((low + high) / 2);
     while (1)
     {
-        if(arr[low]==value) return low;
-        if(arr[high]==value) return high;
-        if(arr[mid]==value) return mid;
-        if(arr[mid] >= value) high = mid;
-        if(arr[mid] < value) low = mid;
-        mid = ceil((low+high)/2);
-        if(low == high || low<0 || high>=Usize) break;
+        if (arr[low] == value)
+            return low;
+        if (arr[high] == value)
+            return high;
+        if (arr[mid] == value)
+            return mid;
+        if (arr[mid] >= value)
+            high = mid;
+        if (arr[mid] < value)
+            low = mid;
+        mid = ceil((low + high) / 2);
+        if (low == high || low < 0 || high >= Usize)
+            break;
     }
     return -1;
-}   
+}
 
-int main(){
+int main()
+{
     int arr[100] = {1, 2, 3, 4};
     int Usize = 4;
     int Tsize = 100;
@@ -91,7 +102,7 @@ int main(){
     printf("The value 1 is at %d index\n", linearSearch(arr, Usize, 1));
     for (int i = 1; i <= 4; i++)
     {
-    printf("The value %d is at %d index\n", i, binarySearch(arr, Usize, i));
+        printf("The value %d is at %d index\n", i, binarySearch(arr, Usize, i));
     }
-     return 0;
+    return 0;
 }
--- qLLst.c
+++ qLLst.c
@@ -1,97 +1,112 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct Node
 {
     int data;
-    struct Node* next;
+    struct Node *next;
 };
 
-void printQueue(struct Node*a) {
-    while(a != NULL){
+void printQueue(struct Node *a)
+{
+    while (a != NULL)
+    {
         printf("Element: %d\n", a->data);
         a = a->next;
     }
 }
 
-struct Node* LLstTraverse(struct Node* a, int index) {
+struct Node *LLstTraverse(struct Node *a, int index)
+{
     int i = 0;
-    while(i < index){
+    while (i < index)
+    {
         a = a->next;
         i++;
     }
     return a;
 }
 
- 
 struct queue
 {
     int size;
     int currrentSize;
-    struct Node* f;
-    struct Node* r;
+    struct Node *f;
+    struct Node *r;
 };
- 
- 
-int isEmpty(struct queue *q){
-    if(q->r==NULL){
+
+int isEmpty(struct queue *q)
+{
+    if (q->r == NULL)
+    {
         return 1;
     }
     return 0;
 }
- 
-int isFull(struct queue *q){
-    if(q->currrentSize==q->size){
+
+int isFull(struct queue *q)
+{
+    if (q->currrentSize == q->size)
+    {
         return 1;
     }
     return 0;
 }
- 
-void enqueue(struct queue *q, int value){
-    if(q->f == NULL){
-        struct Node* new = (struct Node*)malloc(sizeof(struct Node));
+
+void enqueue(struct queue *q, int value)
+{
+    if (q->f == NULL)
+    {
+        struct Node *new = (struct Node *)malloc(sizeof(struct Node));
         new->data = value;
         q->f = new;
         q->r = new;
         q->currrentSize++;
     }
-    else {
-        if(isFull(q)){
+    else
+    {
+        if (isFull(q))
+        {
             printf("This Queue is full\n");
         }
-        else{
-            struct Node* new = (struct Node*)malloc(sizeof(struct Node));
-            new->data = value;new->next = NULL;
+        else
+        {
+            struct Node *new = (struct Node *)malloc(sizeof(struct Node));
+            new->data = value;
+            new->next = NULL;
             q->r->next = new;
             q->r = new;
             q->currrentSize++;
         }
     }
 }
- 
-int dequeue(struct queue *q){
-    if(isEmpty(q)){
+
+int dequeue(struct queue *q)
+{
+    if (isEmpty(q))
+    {
         printf("This Queue is empty\n");
         return -1;
     }
-    else{
-        struct Node* temp = q->f;
+    else
+    {
+        struct Node *temp = q->f;
         q->f = q->f->next;
         q->currrentSize--;
         return temp->data;
     }
 }
 
-
-int main(){
+int main()
+{
     struct queue q;
     q.size = 4;
     q.currrentSize = 0;
     q.f = q.r = NULL;
-    enqueue(q, 5);    
-    enqueue(q, 4);    
-    enqueue(q, 2);    
-    enqueue(q, 8);    
+    enqueue(&q, 5);
+    enqueue(&q, 4);
+    enqueue(&q, 2);
+    enqueue(&q, 8);
 
     return 0;
 }
